a65b502eebfe41d1c9ae069c6d48124e0ca9445f
Junya Oiwa <0920oiwa@gmail.com>
2020-04-07 15:12:50 +0900

    [add]ヘッダー作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,4 +6,9 @@
 
 <style>
 
+html {
+  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "BIZ UDPGothic", Meiryo, sans-serif;
+  color: #282828;
+}
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="index">
-
+    <Header :title="title" :date="updatedAt" />
   </div>
 </template>
 
 <script>
+import Header from '~/components/Header.vue';
 export default {
+  data() {
+    return {
+      title: 'OJのドキュメント置き場について',
+      updatedAt: new Date(),
+    }
+  },
+  components: {
+    Header,
+  }
 
 }
 </script>
