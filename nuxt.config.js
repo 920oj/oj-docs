8bcfa4bc9b112cfd860c06b4f837cd11538cae13
Junya Oiwa <0920oiwa@gmail.com>
2020-04-07 18:02:33 +0900

    [add]metaタグを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,13 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'OJのドキュメント置き場は、一通りまとまった文書を置いて公開できる、自分専用のドキュメントサイトです。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
